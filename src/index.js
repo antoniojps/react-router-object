4d80fac66d4e18f663792baabb6e983e99c92238
António Santos <antoniojps@ua.pt>
2019-08-08 08:17:55 +0100

    Add export RoutesObjContext

--- src/index.js
+++ src/index.js
@@ -5,4 +5,5 @@ export {
   default as mapRoutesObjToArray
 } from './mapRoutesObjToArray/mapRoutesObjToArray'
 export { default as RoutesObjProvider } from './provider/RoutesObjProvider'
+export { RoutesObjContext } from './provider/RoutesObjProvider'
 export { default as withRoutesObj } from './provider/withRoutesObj'
